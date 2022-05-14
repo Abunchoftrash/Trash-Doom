ad8c5960ffbc35b816c5261a59796592e1808024
Abunchoftrash <94553441+Abunchoftrash@users.noreply.github.com>
2022-05-14 12:53:26 +0100

    staged doom upload

---
+++ src/doom/dstrings.c
@@ -0,0 +1,73 @@
+//
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	Globally defined strings.
+// 
+
+
+
+#include "dstrings.h"
+
+const char *doom1_endmsg[] =
+{
+  "Are you sure you want to\nquit this great game?",
+  "Please don't leave, there's more\ndemons to toast!",
+  "Let's beat it -- this is turning\ninto a bloodbath!",
+  "I wouldn't leave if i were you.\ndos is much worse.",
+  "You're trying to say you like dos\nbetter than me, right?",
+  "Don't leave yet -- there's a\ndemon around that corner!",
+  "Ya know, next time you come in here\nI'm gonna toast ya.",
+  "Go ahead and leave. see if I care.",
+};
+
+const char *doom2_endmsg[] =
+{
+  // QuitDOOM II messages
+  "Are you sure you want to\nquit this great game?",
+  "You want to quit?\nthen, thou hast lost an eighth!",
+  "Don't go now, there's a \ndimensional shambler waiting\nat the dos prompt!",
+  "Get outta here and go back\nto your boring programs.",
+  "If I were your boss, I'd \n deathmatch ya in a minute!",
+  "Look, bud. you leave now\nand you forfeit your body count!",
+  "Just leave. when you come\nback, i'll be waiting with a bat.",
+  "You're lucky I don't smack\nyou for thinking about leaving.",
+};
+
+#if 0
+
+// UNUSED messages included in the source release
+
+char* endmsg[] =
+{
+  // DOOM1
+  QUITMSG,
+  // FinalDOOM?
+  "fuck you, pussy!\nget the fuck out!",
+  "you quit and i'll jizz\nin your cystholes!",
+  "if you leave, i'll make\nthe lord drink my jizz.",
+  "hey, ron! can we say\n'fuck' in the game?",
+  "i'd leave: this is just\nmore monsters and levels.\nwhat a load.",
+  "suck it down, asshole!\nyou're a fucking wimp!",
+  "don't quit now! we're \nstill spending your money!",
+
+  // Internal debug. Different style, too.
+  "THIS IS NO MESSAGE!\nPage intentionally left blank."
+};
+
+#endif
+
+  
+
+
